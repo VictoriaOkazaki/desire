4ba5b84c6eb373c0ce4c6552948818949e463cf6
VictoriaOkazaki <victoria.timein@gmail.com>
2022-01-25 16:27:59 +0400

    adaptation for home page added

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,18 @@ $(function () {
     $(".rightside-menu").addClass("rightside-menu--close");
   });
 
+  $(".header__btn-menu").on("click", function () {
+    $(".menu").toggleClass("menu--open");
+  });
+
+  $(".rightside-menu__close").on("click", function () {
+    $(".rightside-menu").addClass("rightside-menu--close");
+  });
+
+  if ($(window).width() < 651) {
+    $('.works-path__item--measurements').appendTo($('.works-path__items-box'));
+  }
+
   $(".top__slider").slick({
     dots: true,
     arrows: false,
