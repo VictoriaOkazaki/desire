363b3f8a8e35dcaa09e741cff8b3ca980b6cf3b6
VictoriaOkazaki <victoria.timein@gmail.com>
2022-01-26 17:28:02 +0400

    blog and contacts pages adaptation added

--- app/js/main.js
+++ app/js/main.js
@@ -22,12 +22,36 @@ $(function () {
     autoplay: true,
   });
 
-  $(".contact-slider").slick({
-    slidesToShow: 10,
-    slidesToScroll: 10,
-    dots: true,
-    arrows: false,
-  });
+  function createSlider(slidesToShow, slidesToScroll) {
+    $(".contact-slider").slick({
+      slidesToShow: slidesToShow,
+      slidesToScroll: slidesToScroll,
+      dots: true,
+      arrows: false,
+    });
+  }
+
+  const width = $(window).width()
+  if (width > 1700) {
+    createSlider(10, 10)
+  } else if (width > 1511) {
+    createSlider(8, 8)
+  } else if (width > 1201) {
+    createSlider(6, 6)
+  } else if (width > 841) {
+    createSlider(4, 4)
+  } else if (width > 551) {
+    createSlider(3, 3)
+  } else if (width > 376) {
+    createSlider(2, 2)
+  } else {
+    $(".contact-slider").slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: false,
+      arrows: false,
+    });
+  }
 
   $(".article-slider__box").slick({
     prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-slide-left.svg" alt="arrow slide left"></button>',
